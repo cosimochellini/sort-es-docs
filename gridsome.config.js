6a9c1f8f317145be802ce679a110ee854289f874
Cosimo <cosimo.chellini@gmail.com>
2020-03-15 12:22:43 +0100

    typo

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,45 +3,47 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-import { resolve } from 'path'
+const path = require('path')
 
 function addStyleResource (rule) {
   rule.use('style-resource')
     .loader('style-resources-loader')
     .options({
       patterns: [
-        resolve(__dirname, './src/assets/scss/globals.scss')
+        path.resolve(__dirname, './src/assets/scss/globals.scss')
       ],
     })
 }
 
-export const siteName = 'sort-es'
-export const siteUrl = 'https://sort-es.netlify.com'
-export const templates = {
-  Doc: '/:slug',
-}
-export const plugins = [
-  {
-    use: '@gridsome/source-filesystem',
-    options: {
-      path: 'docs/**/*.md',
-      typeName: 'Doc',
-      remark: {
-        plugins: [
-          '@gridsome/remark-prismjs'
-        ]
+module.exports = {
+  siteName: 'sort-es',
+  siteUrl: 'https://sort-es.netlify.com',
+  templates: {
+    Doc: '/:slug',
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'docs/**/*.md',
+        typeName: 'Doc',
+        remark: {
+          plugins: [
+            '@gridsome/remark-prismjs'
+          ]
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000
       }
     }
-  },
-  {
-    use: '@gridsome/plugin-sitemap',
-    options: {
-      cacheTime: 600000
-    }
+  ],
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
   }
-]
-export function chainWebpack(config) {
-  const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-  types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
 }
 
